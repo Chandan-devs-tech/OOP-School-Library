e34ad893d335c5dad8d0e93f52631974e92f7f32
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-15 21:03:09 +0530

    Modify student.rb file

--- student.rb
+++ student.rb
@@ -6,6 +6,7 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 
   def play_hooky
