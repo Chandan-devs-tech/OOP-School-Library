7f5eec2a46cb4a0d72be1ea65ebc581eb8555dda
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-22 19:11:17 +0530

    Fix Linters issues

--- specs/book_spec.rb
+++ specs/book_spec.rb
@@ -1,25 +1,25 @@
 require_relative 'spec_helper'
 
 describe Book do
-    before :each do
-        @book = Book.new('Title', 'Author')
-    end
+  before :each do
+    @book = Book.new('Title', 'Author')
+  end
 
-    describe '#new book' do
-        it 'return instance of the Book class' do
-            expect(@book).to be_an_instance_of(Book)
-        end
+  describe '#new book' do
+    it 'return instance of the Book class' do
+      expect(@book).to be_an_instance_of(Book)
     end
+  end
 
-    describe '#title' do
-        it 'return the title of the book' do
-            expect(@book.title).to eql('Title')
-        end
+  describe '#title' do
+    it 'return the title of the book' do
+      expect(@book.title).to eql('Title')
     end
+  end
 
-    describe '#author' do
-        it 'return the author of the book' do
-            expect(@book.author).to eql('Author')
-        end
+  describe '#author' do
+    it 'return the author of the book' do
+      expect(@book.author).to eql('Author')
     end
+  end
 end
--- specs/classroom_spec.rb
+++ specs/classroom_spec.rb
@@ -1,25 +1,25 @@
 require_relative 'spec_helper'
 
 describe Classroom do
-    before :each do
-        @classroom = Classroom.new('C5-Mathematics')
-    end
+  before :each do
+    @classroom = Classroom.new('C5-Mathematics')
+  end
 
-    describe '#new classroom' do
-        it 'return the instance of the Classroom class' do
-            expect(@classroom).to be_an_instance_of(Classroom)
-        end 
+  describe '#new classroom' do
+    it 'return the instance of the Classroom class' do
+      expect(@classroom).to be_an_instance_of(Classroom)
     end
+  end
 
-    describe '#label' do
-        it 'return the label of the classroom' do
-            expect(@classroom.label).to eql('C5-Mathematics')
-        end
+  describe '#label' do
+    it 'return the label of the classroom' do
+      expect(@classroom.label).to eql('C5-Mathematics')
     end
+  end
 
-    describe '#students' do
-        it 'return the student of the classroom' do
-            expect(@classroom.student).to eql []
-        end
+  describe '#students' do
+    it 'return the student of the classroom' do
+      expect(@classroom.student).to eql []
     end
+  end
 end
